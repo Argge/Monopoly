767043fb21dc3520d955f52bba422c0ae25232e1
Argge <argge.yt@gmail.com>
2025-05-28 09:27:45 +0200

    Mini Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 const randomBtn = document.getElementById("randomBtn");
+
+randomBtn.addEventListener("click", () => {
+    let diceNumber = randomDiceNumber();
+    console.log(diceNumber);
+});
+
+
+
+
+function randomDiceNumber() {
+    let random = Math.floor((Math.random()*(7-1))+1);
+    return random;
+}
